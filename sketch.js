9e4e29cc404a20376b7df40d3ab555881448f531
ArthurNepo <126609376+ArthurNepo@users.noreply.github.com>
2023-04-04 13:18:27 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,61 +1,21 @@
-var path,boy, leftBoundary,rightBoundary;
-var pathImg,boyImg;
-var i;
-
-function preload(){
-  pathImg = loadImage("path.png");
-  boyImg = loadAnimation("Runner-1.png","Runner-2.png");
-}
-
-function setup(){
-  
+var weight = [35,38,42,45,43,34,36,41,48,32];
+var soma = 0 
+var media 
+function setup() {
   createCanvas(400,400);
-  
-// Moving background
-path=createSprite(200,200);
-path.addImage(pathImg);
-path.velocityY = 4;
-path.scale=1.2;
 
-//creating boy running
-boy = createSprite(180,340,30,30);
-boy.scale=0.08;
-boy.addAnimation("JakeRunning",boyImg);
-  
-// create left Boundary
-leftBoundary=createSprite(0,0,100,800);
-
-leftBoundary.visible = false;
-
-//create right Boundary
-rightBoundary=createSprite(410,0,100,800);
-rightBoundary.visible = false;
+  for (var i = 0; i < weight.length; i++) {
+    soma=soma + weight[i]
+    
+  }
+  media= soma/weight.length;
+  console.log(media)
 }
 
-function draw() {
-  background(0);
-  path.velocityY = 4;
-  
-  // boy moving on Xaxis with mouse
-  boy.x = World.mouseX;
-  
-  edges= createEdgeSprites();
-  boy.collide(edges[3]);
-  boy.collide(leftBoundary);
-  boy.collide(rightBoundary);
-  
-  //code to reset the background
- /* if(path.y < 400 ){
-    path.y = height*2;
-  } */
-
-     if(path.y > 400 ){
-    path.y = height/2;
-  } 
-
-  /* if(path.y > 400 ){
-    path.y = width/2;
-  } */
-  
-  drawSprites();
+function draw() 
+{
+  background(30);
 }
+
+ 
+
